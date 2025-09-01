0a1be16ee8f936bd8e5beb1cf94e7c8309798304
Mazen Fady <mazenfady85@gmail.com>
2025-09-01 03:06:49 +0300

    stored Proceduers

---
+++ DBFIRST/Program.cs
@@ -0,0 +1,26 @@
+﻿using DBFIRST.Context;  
+namespace DBFIRST
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+             var context = new NorthwindContext() ;
+
+            context.Procedures.DiscontinuedProductsAsync();
+            foreach (var item in context.Employees)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+            var result = await context.Procedures.SelectAllProductsAsync();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            await context.Procedures.DeleteProductByIDAsync(75);
+
+        }
+    }
+}
